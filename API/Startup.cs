b5043c6b2a085bdbb99c7351b7ef673e0ca13d05
zakcore <dreamhunter43@gmail.com>
2023-06-18 09:29:10 +0100

    End Of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -42,6 +42,16 @@ namespace API
 
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
                 });
+
+                services.AddCors(opt=>{
+
+                opt.AddPolicy("CorsPolicy",policy=>{
+
+                policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                }
+                );
+                }
+                );
   
         }
 
@@ -58,7 +68,7 @@ namespace API
            // app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
