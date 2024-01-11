a64fb0e947c9555d2b691d6e27884014e9707fc4
zakcore <dreamhunter43@gmail.com>
2024-01-11 15:47:08 +0100

    datepicker problem

--- client-app/src/features/activities/form/ActivitForm.tsx
+++ client-app/src/features/activities/form/ActivitForm.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent, useState ,useEffect} from 'react'
 
-import { Button, FormField, FormInput, Input, Label, Message, Segment } from 'semantic-ui-react'
+import { Button, FormInput, Input, Label, Message, Segment } from 'semantic-ui-react'
 import { useStore } from '../../../App/stores/store'
 import { observer } from 'mobx-react-lite'
 import { useNavigate, useParams } from 'react-router-dom'
@@ -8,8 +8,13 @@ import { v4 as uuid } from 'uuid';
 
 import LoadingComponent from '../../../App/Layout/LoadingComponent'
 import { Link } from 'react-router-dom'
-import { Formik,Form, Field, ErrorMessage } from 'formik'
+import { Formik,Form, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
+import MyTextInput from '../../../App/common/form/MyTextInput'
+import MyTextArea from '../../../App/common/form/MyTextArea'
+import MySelectInput from '../../../App/common/form/MySelectInput'
+import { categoryOptions } from '../../../App/common/options/categoryOptions'
+import MyDatePicker from '../../../App/common/form/MyDatePicker'
 export default observer( function  ActivityForm (){
     const {activityStore}=useStore();
     let navigateTo = useNavigate();
@@ -28,7 +33,11 @@ const[activity,setActivity]=useState({
 })
 
 const validationschema= Yup.object({
-    title:Yup.string().required("the title is required")
+    title:Yup.string().required("the title is required"),
+    description:Yup.string().required(),
+    category:Yup.string().required(),
+    city:Yup.string().required(),
+    venue:Yup.string().required()
 })
 useEffect(()=>{
     if(id) {loadActivity(id).then(activity=>setActivity(activity!))}else{
@@ -51,23 +60,17 @@ useEffect(()=>{
   
 return(
 <Segment clearing>
-    <Formik validationSchema={validationschema} enableReinitialize initialValues={activity} onSubmit={(values)=>console.log(values)}>
-        {({handleSubmit,errors,touched})=>(
+    <Formik validationSchema={validationschema} enableReinitialize initialValues={activity}
+     onSubmit={(values)=>console.log(values)}>
+        {({handleSubmit})=>(
     <Form className='ui form' onSubmit={handleSubmit}>
-      <FormField>
-
-        <Field placeholder='title' name='title'/>
-          {errors.title && touched.title ? (
-        <Label pointing prompt content={errors.title} />
-
-           ) : null}
-      </FormField>
-
-      <Field placeholder='description'   name='description'  />
-        <Field placeholder='category'  name='category'  />
-        <Field type='date' placeholder='date' name='date' />
-        <Field placeholder='city'   name='city' />
-        <Field placeholder='venue'  name='venue' />
+        <MyTextInput name='title' placeholder='title' />
+        <MyTextArea rows={3} placeholder='description'   name='description'  />
+        <MySelectInput options={categoryOptions} placeholder='category'  name='category'  />
+        <MyDatePicker  placeholderText='date'  name='date'
+         showTimeSelect   />
+        <MyTextInput placeholder='city'   name='city' />
+        <MyTextInput placeholder='venue'  name='venue' />
         <Button  positive loading={loading} floated='right' type='submit' content='Submit'/>
         <Button as={Link} to='/activities' floated='right' type='button' content='Cancel'/>
     </Form>
